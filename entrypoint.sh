2163f9e43aab3397382dff5b763fc94ab0e1209f
Maxime Lebastard <maxime@pathmotion.com>
2019-07-11 18:15:47 +0200

    Add a debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,6 @@ echo "
 aws_access_key_id = $KEY_ID
 aws_secret_access_key = $ACCESS_KEY
 " >  ~/.aws/credentials \
-&& python /app/run.py;
+&& python /app/run.py \
+&& echo $CF_VOLUME_PATH/env_vars_to_export \
+&& cat $CF_VOLUME_PATH/env_vars_to_export;
