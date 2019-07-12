5a00140b534f0dc7861eae20ddf1e74fe06de080
Maxime Lebastard <maxime@pathmotion.com>
2019-07-12 10:09:47 +0200

    Reformat code

--- run.py
+++ run.py
@@ -2,10 +2,11 @@ import boto3
 import json
 import os
 
+# 
 def main():
     secret_name = os.environ["SECRET_NAME"]
     aws_region = os.environ["AWS_REGION"]
-    unseralizedSecret = json.loads(get_secret(secret_name, aws_region))
+    unseralizedSecret = json.loads(fetch_secret(secret_name, aws_region))
     export_secret_to_env(unseralizedSecret)
 
 def export_secret_to_env(secret):
@@ -18,27 +19,21 @@ def export_secret_to_env(secret):
         f.write("{}={}\n".format(key,value))
     f.close()
 
-def get_secret(secret_name, region_name):
-
-    # Create a Secrets Manager client
+def fetch_secret(secret_name, region_name):
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
         region_name=region_name
     )
 
-    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
-    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    # We rethrow the exception by default.
-
-    get_secret_value_response = client.get_secret_value(
+    fetch_secret_value_response = client.fetch_secret_value(
         SecretId=secret_name
     )
 
     # Decrypts secret using the associated KMS CMK.
     # Depending on whether the secret is a string or binary, one of these fields will be populated.
-    if 'SecretString' in get_secret_value_response:
-        return get_secret_value_response['SecretString']
+    if 'SecretString' in fetch_secret_value_response:
+        return fetch_secret_value_response['SecretString']
     else:
         raise NotImplementedError("Binary secrets are not supported. Please set a Key Value secret.")
 
